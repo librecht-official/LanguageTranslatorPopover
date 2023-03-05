91343c723895db2eb0e316c96a823d9bea85d43d
Vladislav Librekht <maclibrecht@gmail.com>
2023-03-05 13:19:37 +0700

    Initial Commit

---
+++ YandexTranslate/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  YandexTranslate
+//
+//  Created by Vladislav Librecht on 05.03.2023.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
